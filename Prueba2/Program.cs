66c10259c47b0ba5572f9c719e0cf7d305c72606
dani_ <dani_@DESKTOP-GHR0T5I>
2024-04-03 13:29:11 -0700

    Agregar archivos de proyecto.

---
+++ Prueba2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Prueba2.UpdatePackageSample modelo = new Prueba2.UpdatePackageSample();
+modelo.monitorFilesLocation="C:\\Users\\dani_\\Desktop\\Carpeta 1";
+modelo.installationFolder="C:\\Users\\dani_\\Desktop\\Carpeta 2";
+modelo.Version="1.0";
+Prueba2.MonitorUpdaterManagerSample.UpdateMonitor(modelo);
---
+++ Prueba2/UpdatePackageSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba2
+{
+    internal class UpdatePackageSample
+    {
+        public string Version { get; set; }
+        public string monitorFilesLocation { get; set; }
+        public string installationFolder { get; set; }
+
+    }
+}
